3cb45eddace7d56ced64c5a9a2e7487688ff55a7
Ryan Petrello <rpetrell@redhat.com>
2021-03-16 13:30:56 -0400

    implement an initial development environment for receptor-based clusters

--- awx/api/serializers.py
+++ awx/api/serializers.py
@@ -4774,6 +4774,7 @@ class InstanceSerializer(BaseSerializer):
     percent_capacity_remaining = serializers.SerializerMethodField()
     jobs_running = serializers.IntegerField(help_text=_('Count of jobs in the running or waiting state that ' 'are targeted for this instance'), read_only=True)
     jobs_total = serializers.IntegerField(help_text=_('Count of all jobs that target this instance'), read_only=True)
+    is_receptor = serializers.SerializerMethodField()
 
     class Meta:
         model = Instance
@@ -4800,6 +4801,7 @@ class InstanceSerializer(BaseSerializer):
             "mem_capacity",
             "enabled",
             "managed_by_policy",
+            "is_receptor",
         )
 
     def get_related(self, obj):
@@ -4816,6 +4818,9 @@ class InstanceSerializer(BaseSerializer):
             return 0.0
         else:
             return float("{0:.2f}".format(((float(obj.capacity) - float(obj.consumed_capacity)) / (float(obj.capacity))) * 100))
+
+    def get_is_receptor(self, obj):
+        return obj.is_receptor()
 
 
 class InstanceGroupSerializer(BaseSerializer):
